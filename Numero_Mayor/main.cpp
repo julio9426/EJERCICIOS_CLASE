e35df197a9d1504a4355a2a5820a9a31b344a2b9
Unknown <julio9426@hotmail.com>
2018-02-15 12:48:01 -0500

    Numero_Mayor
    Algoritmo que imprime el numero mayor

---
+++ Numero_Mayor/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream> //Librerias
+
+using namespace std;
+float N1,N2,N3; //Asignacion de variables
+
+int main(){
+    cout<<"Ingrese Numero 1: ";
+    cin>>N1;
+    cout<<"Ingrese Numero 2: ";
+    cin>>N2;
+    cout<<"Ingrese Numero 3: ";
+    cin>>N3;
+
+    if (N1>N2&&N1>N2){
+        cout<<"El mayor es: "<<N1;
+    }
+    else if (N2>N1&&N2>N3){
+        cout<<"El mayor es: "<<N2;
+    }
+    else{
+        cout<<"El mayor es: "<<N3;
+    }
+    return 0;
+}
